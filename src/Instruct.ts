7878c41d2c541784bdebe8bf777c19da7f34ee95
Steven Roussey <sroussey@gmail.com>
2024-01-03 19:30:33 -0800

    Nit: spacing

--- src/Instruct.ts
+++ src/Instruct.ts
@@ -1,5 +1,5 @@
 //    ****************************************************************************
-//    *   ELMERS: Embedding Language Model Experiential Retrieval Service         *
+//    *   ELMERS: Embedding Language Model Experiential Retrieval Service        *
 //    *                                                                          *
 //    *   Copyright Steven Roussey <sroussey@gmail.com>                          *
 //    *   Licensed under the Apache License, Version 2.0 (the "License");        *
