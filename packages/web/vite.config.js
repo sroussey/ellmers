b5df642e5b77e5568a91ca65bd6d195c1a5f4520
Steven Roussey <sroussey@gmail.com>
2024-04-08 15:57:58 -0700

    chore: update config stuff

--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -4,7 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    target: "esnext",
+  },
   build: {
-    target: "ES2022",
+    target: "esnext",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "esnext",
+    },
   },
 });
