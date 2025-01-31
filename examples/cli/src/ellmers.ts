581de0a226142b7a9113e622f05fc61e689d8469
Steven Roussey <sroussey@gmail.com>
2025-01-31 14:06:33 -0800

    wip: simplify cli to see if i can get built version working

--- examples/cli/src/ellmers.ts
+++ examples/cli/src/ellmers.ts
@@ -10,7 +10,7 @@ import {
   registerMediaPipeTfJsLocalInMemory,
   registerMediaPipeTfJsLocalModels,
 } from "ellmers-test";
-import "ellmers-test";
+import "@huggingface/transformers";
 
 program.version("1.0.0").description("A CLI to run Ellmers.");
 
