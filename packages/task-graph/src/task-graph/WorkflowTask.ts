3298b133e3abe2a6296407e10162ca64db7cd23a
Steven Roussey <sroussey@gmail.com>
2026-05-02 16:27:55 +0000

    refactor(task-graph): extract private WorkflowTask class to its own file

---
+++ packages/task-graph/src/task-graph/WorkflowTask.ts
@@ -0,0 +1,18 @@
+/**
+ * @license
+ * Copyright 2025 Steven Roussey <sroussey@gmail.com>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { GraphAsTask } from "../task/GraphAsTask";
+import type { DataPorts } from "../task/TaskTypes";
+import type { CompoundMergeStrategy } from "./TaskGraphRunner";
+import { PROPERTY_ARRAY } from "./TaskGraphRunner";
+
+export class WorkflowTask<
+  I extends DataPorts,
+  O extends DataPorts,
+> extends GraphAsTask<I, O> {
+  public static override readonly type = "Workflow";
+  public static override readonly compoundMerge = PROPERTY_ARRAY as CompoundMergeStrategy;
+}
